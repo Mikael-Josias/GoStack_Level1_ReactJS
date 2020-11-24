7ec21d8e7875db044f6e152dfc246576465bb377
Mikael Josias <mikaeljosias2002@gmail.com>
2020-11-24 18:06:01 -0300

    feat(app) use axios to get a list of products of a node api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import './App.css';
+import api from './services/api';
+import { render } from 'react-dom';
 
 /**
  * 3 conceitos do react
@@ -11,12 +13,18 @@ import './App.css';
  */
 
 function App(){
-    const [ projects, setProjects ] = useState(['Desenvolvimento de app','Front-end web']);
+    const [ projects, setProjects ] = useState([]);
 
     //useState retorna um vetor de duas posições.
     //
     //1.Variavel com seu valor inicial.
     //2.Função para atualizar o valor inicial.
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data)
+        });
+    }, []);
 
     function handleAddProject() {
         setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -28,7 +36,7 @@ function App(){
         <>
             <Header title="Projects" />
                 <ul>
-                    {projects.map(project => <li key={project}>{project}</li>)}
+                    {projects.map(project => <li key={project.id}>{project.title}</li>)}
                 </ul>
 
                 <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
